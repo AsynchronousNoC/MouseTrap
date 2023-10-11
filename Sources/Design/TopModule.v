ed5bfdfe5401cc31b40ba668f106cf79dd2df9ae
gppchs <giuseppe.chessa@mail.polimi.it>
2023-10-11 23:07:53 +0200

    Modified source with for generate loop, out cases where pipeline stages<2

--- Sources/Design/TopModule.v
+++ Sources/Design/TopModule.v
@@ -1,28 +1,38 @@
 `timescale 1ns / 1ps
 
-module TopModule(
+module TopModule
+    #(
+        parameter PipelineStages=4,
+        parameter WORD_WIDTH =32
+    )
+    (
         input wire req_in_top,
-        input wire[31:0] Data_in__top,
+        input wire[WORD_WIDTH-1:0] Data_in__top,
         output wire ack_in_top,
         
         output wire req_out_top,
-        output wire[31:0] Data_out__top,
+        output wire[WORD_WIDTH-1:0] Data_out__top,
         input wire ack_out_top
     );
-    wire req[5:0];
-    wire[31:0] Data[1:0];
-    wire ack[1:0];
-    
-    //DelayElement delayElem_1(.req_in(req_in_top),.req_out(req[0]));
-    (* DONT_TOUCH = "yes" *) DelayPhysElem delayElem_1(.req_in(req_in_top),.req_out(req[0]));
-    (* DONT_TOUCH = "yes" *) PipelineStage pipeStage_1(.req_in(req[0]),.Data_in(Data_in__top),.ack_in(ack_in_top),.req_out(req[1]),.Data_out(Data[0]),.ack_out(ack[0]));
-    //DelayElement delayElem_2(.req_in(req[1]),.req_out(req[2]));
-    (* DONT_TOUCH = "yes" *) DelayPhysElem delayElem_2(.req_in(req[1]),.req_out(req[2]));
-    (* DONT_TOUCH = "yes" *) PipelineStage pipeStage_2(.req_in(req[2]),.Data_in(Data[0]),.ack_in(ack[0]),.req_out(req[3]),.Data_out(Data[1]),.ack_out(ack[1]));
-    //DelayElement delayElem_3(.req_in(req[3]),.req_out(req[4]));
-    (* DONT_TOUCH = "yes" *) DelayPhysElem delayElem_3(.req_in(req[3]),.req_out(req[4]));
-    (* DONT_TOUCH = "yes" *) PipelineStage pipeStage_3(.req_in(req[4]),.Data_in(Data[1]),.ack_in(ack[1]),.req_out(req[5]),.Data_out(Data_out__top),.ack_out(ack_out_top));
-    //DelayElement delayElem_4(.req_in(req[5]),.req_out(req_out_top));
-    (* DONT_TOUCH = "yes" *) DelayPhysElem delayElem_4(.req_in(req[5]),.req_out(req_out_top));
+    wire req[2*PipelineStages-1:0];
+    wire[WORD_WIDTH-1:0] Data[PipelineStages-2:0];
+    wire ack[PipelineStages-2:0];
+    genvar i;
+       
+    generate
+        //DelayElement delayElem_1(.req_in(req_in_top),.req_out(req[0]));
+        (* DONT_TOUCH = "yes" *) DelayPhysElem delayElem_init(.req_in(req_in_top),.req_out(req[0]));
+        (* DONT_TOUCH = "yes" *) PipelineStage#(.WORD_WIDTH(WORD_WIDTH)) pipeStage_init(.req_in(req[0]),.Data_in(Data_in__top),.ack_in(ack_in_top),.req_out(req[1]),.Data_out(Data[0]),.ack_out(ack[0]));
+        for(i=0;i<PipelineStages-2;i=i+1) begin
+            //DelayElement delayElem_2(.req_in(req[2*i+1]),.req_out(req[2*i++2]));
+            (* DONT_TOUCH = "yes" *) DelayPhysElem delayElem_generic(.req_in(req[2*i+1]),.req_out(req[2*i+2]));
+            (* DONT_TOUCH = "yes" *) PipelineStage#(.WORD_WIDTH(WORD_WIDTH)) pipeStage_generic(.req_in(req[2*i+2]),.Data_in(Data[i]),.ack_in(ack[i]),.req_out(req[2*i+3]),.Data_out(Data[i+1]),.ack_out(ack[i+1]));
+        end
+        (* DONT_TOUCH = "yes" *) DelayPhysElem delayElem_intermediate(.req_in(req[2*(PipelineStages)-3]),.req_out(req[2*(PipelineStages)-2]));
+        (* DONT_TOUCH = "yes" *) PipelineStage#(.WORD_WIDTH(WORD_WIDTH)) pipeStage_final(.req_in(req[2*(PipelineStages)-2]),.Data_in(Data[(PipelineStages-2)]),.ack_in(ack[(PipelineStages-2)]),.req_out(req[2*(PipelineStages)-1]),.Data_out(Data_out__top),.ack_out(ack_out_top));
+        //DelayElement delayElem_4(.req_in(req[(PipelineStages-2)]),.req_out(req_out_top));
+        (* DONT_TOUCH = "yes" *) DelayPhysElem delayElem_final(.req_in(req[2*(PipelineStages)-1]),.req_out(req_out_top));
+    endgenerate
+   
     
 endmodule
