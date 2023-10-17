1c81e321a1567f7d209930fa953a6b5e77e305a8
gppchs <giuseppe.chessa@mail.polimi.it>
2023-10-17 11:22:34 +0200

    Added setup delays and propagation delays

--- Sources/Design/PipelineStage.v
+++ Sources/Design/PipelineStage.v
@@ -15,26 +15,34 @@ module PipelineStage
         output reg[WORD_WIDTH-1:0] Data_out=32'd0,
         input wire ack_out
     );
+    parameter setup_time = 1;
+    parameter propagationTime=1;
     wire control;
+    wire[WORD_WIDTH-1:0] next_Data;
+    wire next_req;
+    wire next_ack;
     
-    /*initial begin
-        ack_in<=0;
-        req_out<=0;
-        Data_out<=32'd0;
-    end*/
+    assign #2 control = ~(ack_out ^ req_out);
+    assign #setup_time next_Data = Data_out;
+    assign #setup_time next_req = req_out;
     
-    assign control = ~(ack_out ^ req_out);
-    
-    always @(control,req_in) begin
+    always @(control,req_in,Data_in) begin
         if (reset == 1) begin 
             ack_in<=0;
             req_out<=0;
             Data_out<=32'd0;
         end
-        else if (control==1) begin
-            Data_out<=Data_in;
-            req_out<=req_in;
-            ack_in <= req_in;
+        else if (reset == 0) begin
+            if (control ==0 ) begin
+                Data_out<= #propagationTime next_Data;
+                req_out<= #propagationTime next_req;
+                ack_in <= #propagationTime next_req;
+             end
+            else if (control==1) begin
+                Data_out<= #propagationTime Data_in;
+                req_out<= #propagationTime req_in;
+                ack_in <= #propagationTime req_in;
+            end
         end
     end
 endmodule 
--- Sources/Simulation/DUT.v
+++ Sources/Simulation/DUT.v
@@ -8,7 +8,7 @@ module DUT(
      parameter WORD_WIDTH=32;
      parameter pipelineStages=10; //sembra un po' inutile nel post implementation
      //TimingParameters
-     parameter outputDelay=1;
+     parameter outputDelay=6;
      parameter inputDelay=1;
      parameter SimDelay=1;
      
@@ -24,23 +24,22 @@ module DUT(
      
      
      TopModule#(pipelineStages,SimDelay,WORD_WIDTH) myModule(reset,req_in_top,Data_in__top,ack_in_top,req_out_top,Data_out__top,ack_out_top);
-     
+     //PipelineStage#(WORD_WIDTH) pipeStage_init(reset,req_in_top,Data_in__top,ack_in_top,req_out_top,Data_out__top,ack_out_top);
      task OutputCheck();  
         begin
             wait(ack_out_top!=req_out_top);
-            #inputDelay;
+            #outputDelay;
             ack_out_top<=req_out_top;
-            #inputDelay;
+            #outputDelay;
         end
      endtask
      
      task InputSend(input[WORD_WIDTH-1:0] Value);  
         begin
             wait(ack_in_top==req_in_top);
+            #3
+            req_in_top =~req_in_top;
             Data_in__top=Value;
-            #2;
-            req_in_top<=~req_in_top;
-            #2;
          end
      endtask
      
@@ -49,20 +48,18 @@ module DUT(
         #20
         reset<=0;
         req_in_top<=0;
-        Data_in__top=32'd25;
         #20
-        req_in_top<=1;
-       #1
-       InputSend(32'd20);
-       InputSend(32'd15);
-       InputSend(32'd128);
-       InputSend(32'd60);
-       InputSend(32'd45);
+        InputSend(32'd20);
+        InputSend(32'd15);
+        InputSend(32'd128);
+        InputSend(32'd60);
+        InputSend(32'd45);
      end
      integer i;
      initial begin
         ack_out_top<=0;
         for (i=0;i<8;i=i+1)
+            //ack_out_top<=0;
             OutputCheck();
      end 
 endmodule
