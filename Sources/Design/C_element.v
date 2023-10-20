7f0710ed5a1e10dbb929b16a8183a60e6e573415
gppchs <giuseppe.chessa@mail.polimi.it>
2023-10-20 18:02:34 +0200

    Verified C_elements configurations and timing
    delay longer than in the paper but with same proportion, could be from implementation

--- Sources/Design/C_element.v
+++ Sources/Design/C_element.v
@@ -1,25 +1,33 @@
 `timescale 1ns/1ps
 
 module C_element(
-		input A,B,
-		output C
+		input A,B,extReset,
+		output C1,C2,C3
 	);
 	//1Poss
-	/*
-	wire Stadio_1,Stadio_2,Stadio_3;
-	assign C =Stadio_1|Stadio_2|Stadio_3;
+	
+	(* DONT_TOUCH = "yes" *) wire Stadio_1,Stadio_2,Stadio_3,C1_old;
+	assign C1 =Stadio_1|Stadio_2|Stadio_3;
 	assign Stadio_1 = A&B;
-	assign Stadio_2 = A&C;
-	assign Stadio_3 = C&B;
-	*/
+	assign Stadio_2 = A&C1_old;
+	assign Stadio_3 = C1_old&B;
+	assign C1_old =C1;
+	
+	
 	
 	
 	//2Poss
-	assign C= (A&B)|(A&C)|(C&B);
+	
+	(* DONT_TOUCH = "yes" *) wire C2_old;
+	assign C2= (A&B)|(A&C2_old)|(C2_old&B);
+	assign C2_old=C2;
+	
 	
 	
 	//3Poss
-	/*
+	wire Clear,Enable;
+	assign Enable=A&B;
+	assign Clear= ~(extReset & (A|B));
 	LDCE #(
         .INIT(1'b0),            // Initial value of latch, 1'b0, 1'b1
         // Programmable Inversion Attributes: Specifies the use of the built-in programmable inversion
@@ -27,12 +35,12 @@ module C_element(
         .IS_G_INVERTED(1'b0)    // Optional inversion for G
     )
     LDCE_inst (
-        .Q(C),     // 1-bit output: Data
-        .CLR(1'b0), // 1-bit input: Asynchronous clear
-        .D(A&B),     // 1-bit input: Data
-        .G(~(A^B)),     // 1-bit input: Gate
+        .Q(C3),     // 1-bit output: Data
+        .CLR(Clear), // 1-bit input: Asynchronous clear
+        .D(1'b1),     // 1-bit input: Data
+        .G(Enable),     // 1-bit input: Gate
         .GE(1'b1)    // 1-bit input: Gate enable
     );
-    */
+    
 	
 	endmodule
--- Sources/Simulation/TestbenchLatch_plus_Celement.v
+++ Sources/Simulation/TestbenchLatch_plus_Celement.v
@@ -4,24 +4,44 @@
 module TestbenchLatch_plus_Celement(
 
     );
-     reg Enable,Reset,Data;
-     wire Q1,Q2;
-     LatchVerilog Latch_1 (Enable, Reset, Data, Q1);
-     latchDir Latch_2(Q1, ~Enable, Reset, Q2);
+     parameter i=1;
      
-     initial begin
-        
-        Reset=1;
-        Enable=0;
-        Data=0;
-        #100
-        Reset=0;
-        Enable=1;
-        Data=1;
-        #1
-        Enable=0;
-        Data=0;
-     
+     if( i==1)begin
+        reg Enable,Reset,Data;
+        wire Q;
+        //LatchVerilog Latch_1 (Enable,Reset, Data, Q1);
+        //latchDir Latch_2(Q1, ~Enable,Reset, Q2);
+        DUT myDUT(Enable,Reset,Data,Q);
+      
+        initial begin
+            Reset=1;
+            Enable=0;
+            Data=0;
+            #100
+            Reset<=0;
+            #10
+            Enable<=1;
+            Data<=1;
+            #20
+            Enable<=0;
+            #1
+            Data<=0;
+        end
      end
      
+     else if( i==2) begin
+        reg A,B,extReset;
+        wire C1,C2,C3;
+        C_element myCelements(A,B,extReset,C1,C2,C3);
+        initial begin
+            extReset=0;
+            A=1;
+            B=1;
+            #50
+            extReset=1;
+            #50;
+            A<=0;
+            B<=0;
+        end
+     end
 endmodule
